c661933574f1ed0e24f2fc18ba9bc4f61fc49a07
Dike Ajah <ekidhaja@gmail.com>
2024-10-13 03:25:17 +0100

    added prettier ignore file

--- __tests__/unit/handlers/sqs-payload-logger.test.ts
+++ __tests__/unit/handlers/sqs-payload-logger.test.ts
@@ -1,26 +1,27 @@
 // Import sqsPayloadLoggerHandler function from sqs-payload-logger.mjs
-import { sqsPayloadLoggerHandler } from '../../../src/handlers/sqs-payload-logger';
-import { jest } from '@jest/globals';
-import { SQSEvent } from 'aws-lambda';
+import { sqsPayloadLoggerHandler } from "../../../src/handlers/sqs-payload-logger";
+import { jest } from "@jest/globals";
+import { SQSEvent } from "aws-lambda";
 
-describe('Test for sqs-payload-logger', function () {
-    it('Verifies the payload is logged', async () => {
+describe("Test for sqs-payload-logger", function () {
+    it("Verifies the payload is logged", async () => {
         // Mock console.log statements so we can verify them.
-        console.info = jest.fn()
+        console.info = jest.fn();
 
         var payload = {
             DelaySeconds: 10,
             MessageAttributes: {
-                "Sender": {
+                Sender: {
                     DataType: "String",
-                    StringValue: "sqs-payload-logger"
-                }
+                    StringValue: "sqs-payload-logger",
+                },
             },
-            MessageBody: "This message was sent by the sqs-payload-logger Lambda function",
-            QueueUrl: "SQS_QUEUE_URL"
-        } as unknown as SQSEvent
+            MessageBody:
+                "This message was sent by the sqs-payload-logger Lambda function",
+            QueueUrl: "SQS_QUEUE_URL",
+        } as unknown as SQSEvent;
 
-        await sqsPayloadLoggerHandler(payload)
-        expect(console.info).toHaveBeenCalledWith(JSON.stringify(payload))
+        await sqsPayloadLoggerHandler(payload);
+        expect(console.info).toHaveBeenCalledWith(JSON.stringify(payload));
     });
 });
--- src/handlers/sqs-payload-logger.ts
+++ src/handlers/sqs-payload-logger.ts
@@ -1,5 +1,5 @@
-import { SQSEvent, Context } from 'aws-lambda';
+import { SQSEvent } from "aws-lambda";
 
 export const sqsPayloadLoggerHandler = async (event: SQSEvent) => {
     console.info(JSON.stringify(event));
-}
+};
