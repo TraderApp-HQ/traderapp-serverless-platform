2623943a7650514ef02586f8484e873ab2f9df2b
Dike Ajah <ekidhaja@gmail.com>
2024-10-13 03:51:55 +0100

    minor edit to git ignore

--- .aws-sam/build/SQSPayloadLogger/dist/sqs-payload-logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sqsPayloadLoggerHandler = void 0;
-const sqsPayloadLoggerHandler = async (event) => {
-    console.info(JSON.stringify(event));
-};
-exports.sqsPayloadLoggerHandler = sqsPayloadLoggerHandler;
--- .aws-sam/build/SQSPayloadLogger/src/handlers/sqs-payload-logger.ts
+++
@@ -1,5 +0,0 @@
-import { SQSEvent } from "aws-lambda";
-
-export const sqsPayloadLoggerHandler = async (event: SQSEvent) => {
-    console.info(JSON.stringify(event));
-};
