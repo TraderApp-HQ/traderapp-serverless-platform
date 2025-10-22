7fca53d31108728d121172b7b265c3ed6b0a15ce
samuel2935 <samuelblessed38@gmail.com>
2025-10-22 22:40:42 +0100

    fix: withdrawal amount

--- src/services/WalletsService/index.ts
+++ src/services/WalletsService/index.ts
@@ -852,7 +852,8 @@ export class WalletsService {
                         switch (body.data.status) {
                             case CryptopayWebhookEventStatus.completed: {
                                 const amount = parseFloat(
-                                    body.data.received_amount ?? "0")
+                                    body.data.received_amount ?? "0"
+                                );
 
                                 const transactionId = body.data.txid ?? "";
 
